30329093f4117e1daea5b737a8614619f606f9cb
Tomás Fernández <tomas.martinez@ing.austral.edu.ar>
2017-09-01 13:57:40 -0300

    fixed laser script

--- src/sensor/example/laser.c
+++ src/sensor/example/laser.c
@@ -6,8 +6,8 @@ void setup() {
 }
 
 void loop(){
-    digitalWrite(pin, HIGH);   // poner el Pin en HIGH
+    digitalWrite(pin, HIGH);   // poner el Pin en HIGH -> prende el laser
     delay(500);               // esperar 5 segundos
-    digitalWrite(pin, LOW);    // poner el Pin en LOW
+    digitalWrite(pin, LOW);    // poner el Pin en LOW -> apaga el 
     delay(500);               // esperar 20 segundos
 }
