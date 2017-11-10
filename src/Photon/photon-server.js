2d4d445de920634e2b1f27d3b3d11caff2fe638a
patrickert <patricio.rickert@gmail.com>
2017-11-10 19:07:25 -0300

    Final version

--- src/Photon/photon-server.js
+++ src/Photon/photon-server.js
@@ -6,21 +6,25 @@ var particle = new Particle();
 
 var deviceId = '27002d000d51353532343635';
 var token = '9e7291e786194c836b9605b5672110bc1918ffcf';
-var content;
+var places;
 
 
   setInterval(function() {
-      particle.getVariable({ deviceId: deviceId, name: 'int_val', auth: token }).then(
+      particle.getVariable({ deviceId: deviceId, name: 'places', auth: token }).then(
           function(data) {
     console.log('Device variable retrieved successfully:', data.body.result);
-    content = data.body.result;
+    places = data.body.result;
   }, function(err) {
     console.log('An error occurred while getting attrs:', err);
   })}, 2000);
 
 server.get('/', function(req, res) {
     console.log('Someone requested the index page');
-    res.send(content);
+    // var random = Math.floor((Math.random() * 20) + 1);
+    res.send({
+        value: places
+    });
+    // res.send(places);
 })
 
 server.listen('3000', function() {
